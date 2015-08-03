4996f5b4270fd161969f7b53ae70ccc05ebc1270
Vijay <vijay.verma@dotsquares.com>
2015-08-03 11:36:13 +0530

    debug install script

--- bash_profile
+++ bash_profile
@@ -1,1 +1,1 @@
-source ~/.bashrc  "tmux loads bash_profile, so this will load our bash setting into tmux session
+source ~/.bashrc  
--- install.sh
+++ install.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 #This file is to create symlink for dotfiles in home directory
-dir='~/.dotfiles'
-old_dir='~/.old_dotfiles'
-echo 'creating $old_dir folder for backup'
+dir=~/dotfiles
+old_dir=~/old_dotfiles
+echo "creating $old_dir folder for backup"
 mkdir -p $old_dir
-echo '......done'
-files='bashrc bash_profile vimrc'
+echo "......done"
+files=".bashrc .bash_profile .vimrc .config/sublime-text-3/Packages/User/Preferences.sublime-settings"
 
-echo 'changing to $dir folder'
+echo "changing to $dir folder"
 cd $dir
-echo '.......done'
+echo ".......done"
 
 #move any dotfile to backup and dotfiles folder
 for file in $files
 	do
-		echo 'moving $file in $old_dir'
-		mv ~/.$file $old_dir
-		echo 'creating symlink'
-		ln -s $dir/$file ~/.$file
+		echo "moving $file in $old_dir"
+		mv ~/$file $old_dir
+		echo "creating symlink"
+		ln -s $dir/$file ~/$file
 	done
-	
+
 
 
