eca5dcce7ed54871a839f345611cc1f9347856eb
Ravins <ravin.singh1990@gmail.com>
2015-08-04 05:45:35 +0530

    touch file if not exists

--- install.sh
+++ install.sh
@@ -14,6 +14,11 @@ echo ".......done"
 #move any dotfile to backup and dotfiles folder
 for file in $files
 	do
+		#check if file exists otherwise create blank one
+		if [ ! -f ~/.$file ];then
+			echo "creating ~/.$file file"
+			touch ~/.$file
+		fi
 		echo "moving $file in $old_dir"
 		mv ~/.$file $old_dir
 		echo "creating symlink"
